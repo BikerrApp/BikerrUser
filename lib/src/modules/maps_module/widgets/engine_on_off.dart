93f0f23114d47faf1c0feddf938008df87f8ef50
Khushvinder singh <khushbaansal@gmail.com>
2023-11-25 23:34:13 +0530

    fix minor issue

--- lib/src/modules/maps_module/widgets/engine_on_off.dart
+++ lib/src/modules/maps_module/widgets/engine_on_off.dart
@@ -16,62 +16,60 @@ class EngineOnOffBtnComp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      return GestureDetector(
-        onTap: () {
-          dialogBox(
-            title: bmc.mapC.dc.isEngineOn.value ? "Engine Off" : "Engine On",
-            centerChild: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const ThinTextComp(
-                  data: "Turn Engine",
-                  isCenter: true,
-                ),
-                15.height,
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    RedButtonComp(
-                      btnName: "ON",
-                      isChange: true,
-                      onTap: () async {
-                        await bmc.mapC.dc.sendEngineCommands(cmd: "engineResume");
-                      },
-                      width: 100,
-                      isLoading: bmc.mapC.dc.isEngineCommandChange,
-                    ),
-                    10.width,
-                    RedButtonComp(
-                      btnName: "OFF",
-                      onTap: () async {
-                        await bmc.mapC.dc.sendEngineCommands(cmd: "engineStop");
-                      },
-                      width: 100,
-                      isLoading: bmc.mapC.dc.isEngineCommandChange,
-                    ),
-                  ],
-                ),
-                15.height,
-                GestureDetector(
-                  onTap: () {
-                    Get.back();
-                  },
-                  child: const MediumTextComp(
-                    data: "Cancel",
-                    size: 15,
+    return GestureDetector(
+      onTap: () {
+        dialogBox(
+          title: bmc.mapC.dc.isEngineOn.value ? "Engine Off" : "Engine On",
+          centerChild: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const ThinTextComp(
+                data: "Turn Engine",
+                isCenter: true,
+              ),
+              15.height,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RedButtonComp(
+                    btnName: "ON",
+                    isChange: true,
+                    onTap: () async {
+                      await bmc.mapC.dc.sendEngineCommands(cmd: "engineResume");
+                    },
+                    width: 100,
+                    isLoading: bmc.mapC.dc.isEngineCommandChange,
                   ),
-                )
-              ],
-            ),
-          );
-        },
-        child: Image.asset(
-          engineIcon,
-          color: whiteColor,
-          height: 30.h,
-        ),
-      );
-    });
+                  10.width,
+                  RedButtonComp(
+                    btnName: "OFF",
+                    onTap: () async {
+                      await bmc.mapC.dc.sendEngineCommands(cmd: "engineStop");
+                    },
+                    width: 100,
+                    isLoading: bmc.mapC.dc.isEngineCommandChange,
+                  ),
+                ],
+              ),
+              15.height,
+              GestureDetector(
+                onTap: () {
+                  Get.back();
+                },
+                child: const MediumTextComp(
+                  data: "Cancel",
+                  size: 15,
+                ),
+              )
+            ],
+          ),
+        );
+      },
+      child: Image.asset(
+        engineIcon,
+        color: whiteColor,
+        height: 30.h,
+      ),
+    );
   }
 }
